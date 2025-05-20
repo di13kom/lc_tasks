0194cdf2f95069c79d9f917b91e032b771c97657
di13kom <di13kom@gmail.com>
2025-05-20 21:23:41 +0400

    Add solution 88

---
+++ src/Problems/88/Solution.cs
@@ -0,0 +1,29 @@
+namespace LeetCode.Problems._88;
+
+public class Solution
+{
+    public void Merge(int[] nums1, int m, int[] nums2, int n)
+    {
+        int sumCount = m + n;
+        while (sumCount > 0)
+        {
+            if (n <= 0)
+            {
+                nums1[--sumCount] = nums1[--m];
+            }
+            else if (m <= 0)
+            {
+                nums1[--sumCount] = nums2[--n];
+            }
+            else if (nums1[m - 1] > nums2[n - 1])
+            {
+                nums1[--sumCount] = nums1[--m];
+            }
+            else
+            {
+                nums1[--sumCount] = nums2[--n];
+            }
+
+        }
+    }
+}
